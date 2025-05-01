3b8b383a0c10b624ee39bbfe4eb8a700800aa3ec
Edward Chen <38011964+apurplecaboose@users.noreply.github.com>
2025-05-01 17:26:14 +0100

    shit not working initial commits

---
+++ WOL Unity/Assets/PacketSender.cs
@@ -0,0 +1,53 @@
+using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+
+public class PacketSender : MonoBehaviour
+{
+    public string TestMacAddress;
+    public void SendMagicPacket(string macAddress, string broadcastAddress = "255.255.255.255", int port = 9)
+    {
+        byte[] macBytes = ParseMacAddress(macAddress);
+
+        // Create the magic packet
+        byte[] packet = new byte[102];
+        for (int i = 0; i < 6; i++) packet[i] = 0xFF; // Start with 6 bytes of 0xFF
+        for (int i = 0; i < 16; i++) macBytes.CopyTo(packet, 6 + i * 6); // Append MAC 16 times
+
+        // Send the magic packet via UDP
+        using (UdpClient udpClient = new UdpClient())
+        {
+            udpClient.EnableBroadcast = true;
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse(broadcastAddress), port);
+            udpClient.Send(packet, packet.Length, endpoint);
+        }
+    }
+    byte[] MacAddressinBytes(string[] processedMacAddress)
+    {
+        byte[] macBytes = new byte[6];
+        return macBytes;
+    }
+    byte[] ParseMacAddress(string macAddress)
+    {
+        string[] macParts = macAddress.Split(':');//splits strings and removes all colons
+        byte[] macBytes = new byte[6];
+        for (int i = 0; i < 6; i++)
+        {
+            macBytes[i] = Convert.ToByte(macParts[i], 16);
+        }
+        return macBytes;
+    }
+    void Start()
+    {
+        byte[] printoutput = ParseMacAddress(TestMacAddress);
+        foreach (var something in printoutput)
+        {
+            Debug.Log(something);
+        }
+
+    }
+}
+
