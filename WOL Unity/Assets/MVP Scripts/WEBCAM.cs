7f8a97affafaac971fcf3df2cf70ec302bdebf02
apurplecaboose <38011964+apurplecaboose@users.noreply.github.com>
2025-05-07 17:26:37 +0100

    capture card live display unity 6

---
+++ WOL Unity/Assets/MVP Scripts/WEBCAM.cs
@@ -0,0 +1,78 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WEBCAM : MonoBehaviour
+{
+    [SerializeField] RawImage _rawImage; // Serialized RawImage instance
+    [SerializeField] TMP_Dropdown _Dropdown;
+    [SerializeField] Texture _NullImage; // Fallback image when webcam fails
+    private WebCamTexture _webcamTexture;
+    private List<string> _deviceNames = new List<string>();
+    public GameObject Toggle;
+
+    void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 120; // Set to desired FPS
+    }
+
+    void Start()
+    {
+        RefreshWebcamDevices();
+
+        if (_deviceNames.Count > 0)
+        {
+            ChangeCamera(0); // Start with the first device
+        }
+        else
+        {
+            Debug.LogWarning("No webcam devices found. Using fallback texture.");
+            _rawImage.texture = _NullImage;
+        }
+
+        // Listen for dropdown value changes
+        _Dropdown.onValueChanged.AddListener(ChangeCamera);
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Toggle.SetActive(!Toggle.activeSelf);
+        }
+    }
+    void ChangeCamera(int index)
+    {
+        if (_webcamTexture != null)
+        {
+            _webcamTexture.Stop();
+        }
+        _webcamTexture = new WebCamTexture(_deviceNames[index]);
+        _webcamTexture.Play();
+        _rawImage.texture = _webcamTexture;
+    }
+    public void RefreshWebcamDevices()
+    {
+        _deviceNames.Clear();
+        _Dropdown.ClearOptions();
+
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        if (devices.Length > 0)
+        {
+            foreach (var device in devices)
+            {
+                _deviceNames.Add(device.name);
+            }
+
+            _Dropdown.AddOptions(_deviceNames);
+        }
+    }
+
+
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+}
