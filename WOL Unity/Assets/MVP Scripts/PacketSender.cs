c5e67ee096c70bc5e2657371c2e65dad0d4355ad
apurplecaboose <38011964+apurplecaboose@users.noreply.github.com>
2025-05-08 17:53:09 +0100

    packet sender

--- WOL Unity/Assets/MVP Scripts/PacketSender.cs
+++ WOL Unity/Assets/MVP Scripts/PacketSender.cs
@@ -4,89 +4,94 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
-using UnityEngine.TextCore.Text;
-using UnityEngine.Events;
 using System.Net.NetworkInformation;
+using System.Threading.Tasks;
 
+//public class WOL_DeviceInstance
+//{
+//    public string MAC;
+//    public string Ping_IP;
+//    public string[] SitesToLoad;
+//}
 public class PacketSender : MonoBehaviour
 {
-    public string TestMacAddress;
-    public string IPadress;
+    [SerializeField] string _pingip, _mac;
+    [SerializeField] string[] _sitestoload;
+    [SerializeField] bool _quitafterlaunch;
+    void Start()
+    {
+        //check if there is json data
+        //if is load as devices
+        //if not load settings scene
+
+    }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+
+    }
+    void SendMagicPacket(string macAddress, string broadcastAddress = "255.255.255.255", int port = 9)
+    {
+        byte[] macBytes = ParseMacAddress(macAddress);
+
+        // Create the magic packet
+        byte[] packet = new byte[102];
+        for (int i = 0; i < 6; i++) packet[i] = 0xFF; // Start with 6 bytes of 0xFF
+        for (int i = 0; i < 16; i++) macBytes.CopyTo(packet, 6 + i * 6); // Append MAC 16 times
+
+        // Send the magic packet via UDP
+        using (UdpClient udpClient = new UdpClient())
         {
-            SimplePing(IPadress, 1000);
+            udpClient.EnableBroadcast = true;
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse(broadcastAddress), port);
+            udpClient.Send(packet, packet.Length, endpoint);
         }
     }
-    //public void SendMagicPacket(string macAddress, string broadcastAddress = "255.255.255.255", int port = 9)
-    //{
-    //    byte[] macBytes = ParseMacAddress(macAddress);
-
-    //    // Create the magic packet
-    //    byte[] packet = new byte[102];
-    //    for (int i = 0; i < 6; i++) packet[i] = 0xFF; // Start with 6 bytes of 0xFF
-    //    for (int i = 0; i < 16; i++) macBytes.CopyTo(packet, 6 + i * 6); // Append MAC 16 times
-
-    //    // Send the magic packet via UDP
-    //    using (UdpClient udpClient = new UdpClient())
-    //    {
-    //        udpClient.EnableBroadcast = true;
-    //        IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse(broadcastAddress), port);
-    //        udpClient.Send(packet, packet.Length, endpoint);
-    //    }
-    //}
-    //byte[] MacAddressinBytes(string[] processedMacAddress)
-    //{
-    //    byte[] macBytes = new byte[6];
-    //    return macBytes;
-    //}
-    //byte[] ParseMacAddress(string macAddress)
-    //{
-    //    string[] macParts = macAddress.Split(':');//splits strings and removes all colons
-    //    byte[] macBytes = new byte[6];
-    //    for (int i = 0; i < 6; i++)
-    //    {
-    //        macBytes[i] = Convert.ToByte(macParts[i], 16);
-    //    }
-    //    return macBytes;
-    //}
-    //public static bool PingHost(string host)
-    //{
-    //    try
-    //    {
-    //        System.Net.NetworkInformation.Ping ping = new System.Net.NetworkInformation.Ping();
-    //        PingReply reply = ping.Send(host, 1000); // Timeout set to 1000ms
-    //        return reply.Status == IPStatus.Success;
-    //    }
-    //    catch
-    //    {
-    //        return false; // Handle exceptions (e.g., invalid host or network issues)
-    //    }
-    //}
-    public static bool SimplePing(string host, int timeout)
+    byte[] ParseMacAddress(string macAddress)
+    {
+        string[] macParts = macAddress.Split(':');//splits strings and removes all colons
+        byte[] macBytes = new byte[6];
+        for (int i = 0; i < 6; i++)
+        {
+            macBytes[i] = Convert.ToByte(macParts[i], 16);
+        }
+        return macBytes;
+    }
+    bool SimplePing(string host, int timeout)
     {
         System.Net.NetworkInformation.Ping newsimpleping = new System.Net.NetworkInformation.Ping();
         PingReply reply = newsimpleping.Send(host, timeout);
-        if (reply.Status == IPStatus.Success)
+        if (reply.Status == IPStatus.Success) return true;
+        else return false;
+    }
+    async void CheckServerStatus()
+    {
+        int delay = 500;
+        bool serveronlinestatus = false;
+        do
         {
-            print("Ping YAY");
-            return true;
+            serveronlinestatus = SimplePing(_pingip, delay);
+            await Task.Delay(delay + 5);
+        }
+        while (!serveronlinestatus);
+        LaunchSitesToLoad(_quitafterlaunch);
+        return;
+    }
+    void LaunchSitesToLoad(bool quitoncompletion)
+    {
+        foreach (string site in _sitestoload)
+        {
+            Application.OpenURL(site);
+        }
+        if(quitoncompletion)
+        {
+            Application.Quit();
+            return;
         }
         else
         {
-            print("Ping SAD");
-            return false;
+            //instantiate confirmation
         }
     }
-    //void Start()
-    //{
-    //    byte[] printoutput = ParseMacAddress(TestMacAddress);
-    //    foreach (var something in printoutput)
-    //    {
-    //        Debug.Log(something);
-    //    }
 
-    //}
 }
 
